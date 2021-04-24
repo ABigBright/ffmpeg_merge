c6c03d0d3ba9cf566cf7fdccc93519b77fabd1bd
briq <990647625@qq.com>
2021-04-24 22:35:52 +0800

    feat : add flist_example.txt

--- ffmpeg_download.sh
+++ ffmpeg_download.sh
@@ -7,7 +7,7 @@ do
     echo -e '\e[31mStart Downloading  => \e[32m'$file
     ffmpeg -i $file ${file:33:7}
     echo -e '\e[33mFinish Downloading => \e[32m'$file
-    echo ${file:33:7} >> flist.txt
+    echo file \'`pwd`/${file:33:7}\' >> flist.txt
 done
 
 ffmpeg -f concat -safe 0 -i flist.txt -c copy output.mp4
