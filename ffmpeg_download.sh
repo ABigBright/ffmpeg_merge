73be67cf2fe315ec75da8c502dded50620928171
briq <990647625@qq.com>
2021-04-24 22:32:55 +0800

    feat : update the shell scripts

--- ffmpeg_download.sh
+++ ffmpeg_download.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+touch flist.txt
+
 for file in $(cat fsrc.txt)
 do
-    echo -e '\e[31mStart Downloading     => \e[32m'$file
+    echo -e '\e[31mStart Downloading  => \e[32m'$file
     ffmpeg -i $file ${file:33:7}
-    echo -e '\e[33m=> Finish Downloading => \e[32m'$file
+    echo -e '\e[33mFinish Downloading => \e[32m'$file
+    echo ${file:33:7} >> flist.txt
 done
 
+ffmpeg -f concat -safe 0 -i flist.txt -c copy output.mp4
+
+echo -e '\e[33m=> Finish Video Merge\e[0m'
